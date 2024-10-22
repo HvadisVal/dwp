13ff82e13593c5a305714845870f17fb492f4069
HvadisVal <114469812+HvadisVal@users.noreply.github.com>
2024-10-22 11:16:00 +0200

    mock data for database and movie display

--- dbcon.php
+++ dbcon.php
@@ -4,7 +4,7 @@ $pass = "";
 
 function dbCon ($user, $pass) {
     try {
-        $dbCon = new PDO('mysql:host=localhost; dbname=crud; charset=utf8', $user, $pass);
+        $dbCon = new PDO('mysql:host=localhost; dbname=dwp; charset=utf8', $user, $pass);
         return $dbCon;
     } catch (PDOException $err) {
         echo 'Connection failed: ' . $err->getMessage() . '<br>';
