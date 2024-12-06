8b3198532715c84146b71b160bd61ad979b9169c
Yevhenii <yevmot01@easv365.dk>
2024-12-06 13:38:22 +0100

    fetch error fixed

--- frontend/controllers/FetchController.php
+++ frontend/controllers/FetchController.php
@@ -5,16 +5,24 @@ class FetchController {
     private $model;
 
     public function __construct($connection) {
-        // Pass the connection to the AboutModel constructor
+        // Pass the connection to the MoviesModel constructor
         $this->model = new MoviesModel($connection);
     }
 
     public function fetchMovies($data) {
+        // Get the optional parameters from the input data
         $date = $data['date'] ?? null;
         $movieId = $data['movieId'] ?? null;
         $versionId = $data['versionId'] ?? null;
 
-        // Fetch filtered movies using the model
-        return $this->model->getFilteredMovies($date, $movieId, $versionId);
+        // Fetch filtered movies using the model method
+        $movies = $this->model->getFilteredMovies($date, $movieId, $versionId);
+
+        // Ensure that the movies are returned as an array (empty array if no movies found)
+        if (empty($movies)) {
+            return []; // Return an empty array if no movies are found
+        }
+
+        return $movies; // Return the fetched movies
     }
 }
