667bd3859e9acbe6e8661ee56ca0073d565c6691
Oleh <118124646+Olehq312@users.noreply.github.com>
2024-12-11 10:45:54 +0100

    option of next and previous week has been added

--- frontend/views/movies/movies_content.php
+++ frontend/views/movies/movies_content.php
@@ -13,68 +13,62 @@ $navbar = new NavbarController();
 $navbar->handleRequest();
  ?>
  
-<div class="container">
-    <?php foreach ($moviesById as $movieId => $movieData): ?>
-        <div class="movie-container">
-            <!-- Movie Information -->
-            <div class="movie-info">
-    <!-- Display poster image -->
-    <?php if (!empty($movieData['details']['PosterPath'])): ?>
-            <div class="movie-poster">
-            <a href="/dwp/movie?movie_id=<?= urlencode($movieId); ?>">
-                <?php foreach ($movieData['details']['PosterPath'] as $imagePath): ?>
-                    <img src="<?= htmlspecialchars($imagePath); ?>" alt="<?= htmlspecialchars($movieData['details']['Title']); ?>" class="poster">
-                <?php endforeach; ?>
-            </a>     
-            </div> 
-    <?php endif; ?>
+ 
+ <div class="container">
+    <div class="week-navigation">
+        <?php if ($offset > 0): ?>
+            <a href="?week_offset=<?= $offset - 1 ?>" id="prev-week" class="nav-button">❮ Previous Week</a>
+        <?php endif; ?>
+        <a href="?week_offset=<?= $offset + 1 ?>" id="next-week" class="nav-button">Next Week ❯</a>
+    </div>
 
-    <div class="movie-details">
-        <h4><?= htmlspecialchars($movieData['details']['Title']); ?></h4>
-        <p>Duration: <?= htmlspecialchars($movieData['details']['Duration']); ?></p>
-        <p>Rating: <?= htmlspecialchars($movieData['details']['Rating']); ?></p>
-        <p>Age Limit: Allowed for children over <?= htmlspecialchars($movieData['details']['Rating']); ?> years</p>
+    <div id="schedule-container">
+        <?php foreach ($moviesById as $movieId => $movieData): ?>
+            <div class="movie-container">
+                <!-- Movie Information -->
+                <div class="movie-info">
+                    <?php if (!empty($movieData['details']['PosterPath'])): ?>
+                        <div class="movie-poster">
+                            <a href="/dwp/movie?movie_id=<?= urlencode($movieId); ?>">
+                                <?php foreach ($movieData['details']['PosterPath'] as $imagePath): ?>
+                                    <img src="<?= htmlspecialchars($imagePath); ?>" alt="<?= htmlspecialchars($movieData['details']['Title']); ?>" class="poster">
+                                <?php endforeach; ?>
+                            </a>     
+                        </div> 
+                    <?php endif; ?>
+
+                    <div class="movie-details">
+                        <h4><?= htmlspecialchars($movieData['details']['Title']); ?></h4>
+                        <p>Duration: <?= htmlspecialchars($movieData['details']['Duration']); ?></p>
+                        <p>Rating: <?= htmlspecialchars($movieData['details']['Rating']); ?></p>
+                        <p>Age Limit: Allowed for children over <?= htmlspecialchars($movieData['details']['Rating']); ?> years</p>
+                    </div>
+                </div>
+
+                <!-- Movie Showtimes by Date (Vertically) -->
+                <div class="schedule">
+                    <?php foreach ($allDates as $date): ?>
+                        <div class="date-column">
+                            <div class="date-header">
+                                <?= date("D, d/m", strtotime($date)); ?>
+                            </div>
+                            <?php if (isset($movieData['showtimes'][$date])): ?>
+                                <?php foreach ($movieData['showtimes'][$date] as $showtime): ?>
+                                    <a href="/dwp/frontend/controllers/initialize_booking.php?movie_id=<?= htmlspecialchars($showtime['Movie_ID']); ?>&cinema_hall_id=<?= htmlspecialchars($showtime['CinemaHall_ID']); ?>&time=<?= urlencode($showtime['ShowTime']); ?>&date=<?= urlencode($showtime['ShowDate']); ?>" class="showtime-card">
+                                        <h6><?= htmlspecialchars($showtime['CinemaHall_Name']); ?></h6>
+                                        <h6><?= date("g:i a", strtotime($showtime['ShowTime'])); ?></h6>
+                                        <div class="version"><?= htmlspecialchars($showtime['Version'] ?? 'Standard'); ?></div>
+                                    </a>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <div class="no-showtimes"></div>
+                            <?php endif; ?>
+                        </div>
+                    <?php endforeach; ?>
+                    
+                </div>
+                
+            </div>
+        <?php endforeach; ?>
     </div>
 </div>
-
-
-
-
-             <!-- Movie Showtimes by Date (Vertically) -->
-             <div class="schedule">
-                <?php foreach ($allDates as $date): ?>
-                    <div class="date-column">
-                        <div class="date-header">
-                            <?= date("D, d/m", strtotime($date)); ?>
-                        </div>
-                        <?php if (isset($movieData['showtimes'][$date])): ?>
-                            <?php foreach ($movieData['showtimes'][$date] as $showtime): ?>
-
-                                <a href="/dwp/frontend/controllers/initialize_booking.php?movie_id=<?= htmlspecialchars($showtime['Movie_ID']); ?>&cinema_hall_id=<?= htmlspecialchars($showtime['CinemaHall_ID']); ?>&time=<?= urlencode($showtime['ShowTime']); ?>&date=<?= urlencode($showtime['ShowDate']); ?>" class="showtime-card">
-    <h6><?= htmlspecialchars($showtime['CinemaHall_Name']); ?></h6>
-    <h6><?= date("g:i a", strtotime($showtime['ShowTime'])); ?></h6>
-    <div class="version"><?= htmlspecialchars($showtime['Version'] ?? 'Standard'); ?></div>
-</a>
-<?php
-$link = "/dwp/frontend/controllers/initialize_booking.php?movie_id=" . htmlspecialchars($showtime['Movie_ID']) . 
-    "&cinema_hall_id=" . htmlspecialchars($showtime['CinemaHall_ID']) . 
-    "&time=" . urlencode($showtime['ShowTime']) . 
-    "&date=" . urlencode($showtime['ShowDate']);
-?>
-
-
-
-                            <?php endforeach; ?>
-                        <?php else: ?>
-                            <div class="no-showtimes"></div>
-                        <?php endif; ?>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-    <?php endforeach; ?>
-</div>
-
-
-</body>
-</html>
